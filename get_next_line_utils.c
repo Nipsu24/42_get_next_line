fd6bb44c4d0090bf6d27d4529280680e06f73a9d
Marius Meier <marius.meier24@gmail.com>
2024-01-10 16:32:43 +0200

    add updated strjoin

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 11:47:22 by mmeier            #+#    #+#             */
-/*   Updated: 2024/01/08 15:18:46 by mmeier           ###   ########.fr       */
+/*   Updated: 2024/01/10 16:19:45 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,31 +35,27 @@ char	*ft_strchr(const char *s, int c)
 	return (0);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_gnl_strjoin(char *s1, char *s2)
 {
 	char	*ptr;
 	int		s1len;
 	int		s2len;
-	int		i;
-	int		j;
+	char	*result;
 
-	i = 0;
-	j = 0;
 	s1len = ft_strlen(s1);
 	s2len = ft_strlen(s2);
 	ptr = (char *) malloc ((s1len + s2len + 1) * sizeof(char));
 	if (ptr == 0)
+	{
+		free (s1);
+		free (s2);
 		return (NULL);
-	while (s1[i] != '\0')
-	{
-		ptr[i] = s1[i];
-		i++;
 	}
-	while (s2[j] != '\0')
-	{
-		ptr[i + j] = s2[j];
-		j++;
-	}
-	ptr[i + j] = '\0';
-	return (ptr);
+	result = ptr;
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
+	*ptr = '\0';
+	return (result);
 }
